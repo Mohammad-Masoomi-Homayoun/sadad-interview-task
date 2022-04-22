9a0fa7a6834ad8e0e466f3a553a962e604d3d536
Mohammad Masoomi <en.masoomi@gmail.com>
2022-04-22 18:12:21 +0430

    Initial commit

---
+++ src/test/java/ir/bmi/baam/interviewassignment/InterviewAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.bmi.baam.interviewassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
